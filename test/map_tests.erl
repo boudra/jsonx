6e110068e4b74612a273b84e921ae10901944663
Mohamed Boudra <boudra.moha@gmail.com>
2018-04-04 18:54:41 +0200

    Revert "removing the tuple around maps and returning them straight"
    This reverts commit 5fd80cf0407b9e20dd2f93ff82ec0bbc2c6d60a0.

--- test/map_tests.erl
+++ test/map_tests.erl
@@ -5,12 +5,12 @@
 encl0_test() -> <<"{}">> = jsonx:encode(#{}).
 encl1_test() -> <<"{}">> = jsonx:encode(#{}).
 encl2_test() -> <<"{\"a\":{\"b\":{\"c\":3}}}">> = jsonx:encode(#{a=>#{b=>#{c=>3}}}).
-encl3_test() -> <<"{\"a\":2,\"b\":3,\"c\":4,\"d\":5}">> = jsonx:encode(#{a=>2, b=>3, c=>4, d=>5}).
+encl3_test() -> <<"{\"a\":2,\"b\":3,\"c\":4,\"d\":5}">> = jsonx:encode(#{a=>2, b=>3, c=>4, d=>5}). 
 
 %%% Test decode map
 decarr0_test() ->
-	#{} = jsonx:decode(<<"{}">>, [{format, map}]).
+	{map, #{}} = jsonx:decode(<<"{}">>, [{format, map}]).
 decarr1_test() ->
-	#{<<"a">> := 2, <<"b">> := 3, <<"c">> := 4, <<"d">> := 5} =
+	{map,#{<<"a">> := 2, <<"b">> := 3, <<"c">> := 4, <<"d">> := 5}} =
 	jsonx:decode(<<"{\"a\":2,\"b\":3,\"c\":4,\"d\":5}">>, [{format, map}]).
 -endif.
